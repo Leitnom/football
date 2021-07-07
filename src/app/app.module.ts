4f1bd7a434aae3cd01a4c24a8f1274617840fd6c
Juan Montiel <juan123m61@gmail.com>
2021-07-07 06:56:53 -0500

    Creamos los componentes header, leagues, teams, players

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LeaguesComponent } from './pages/leagues/leagues.component';
+import { TeamsComponent } from './pages/teams/teams.component';
+import { PlayersComponent } from './pages/players/players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    LeaguesComponent,
+    TeamsComponent,
+    PlayersComponent,
+
   ],
   imports: [
     BrowserModule

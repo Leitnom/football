3785b16b2305baac0caefd16153abc2171dd09b0
Juan Montiel <juan123m61@gmail.com>
2021-07-07 08:01:15 -0500

    Montramos Teams en la vista y agregamos estilos

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FootballService } from 'src/app/services/football.service';
 
 @Component({
   selector: 'app-teams',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamsComponent implements OnInit {
 
-  constructor() { }
+  teams: any;
+
+  constructor(private football: FootballService) { 
+    this.football.getTeams().subscribe((data: any) => {
+      this.teams = data;
+    }, (error) => {
+      console.log(error);
+    });
+
+  }
 
   ngOnInit(): void {
   }

2b06b5eb5638127d76b6a14afc61eca534d3a886
Juan Montiel <juan123m61@gmail.com>
2021-07-07 10:30:01 -0500

    Finalizamos peticion de detalles de team

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FootballService } from 'src/app/services/football.service';
 
 @Component({
   selector: 'app-team',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  teams: any;
+  players: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private football: FootballService) {
+    this.activatedRoute.params.subscribe(params => {
+      this.football.getTeamById(params.id).subscribe((data: any) => {
+        this.teams = data;
+      }, (error) => {
+        console.log(error);
+      });
+
+      this.football.getPlayersById(params.id).subscribe((data: any) => {
+        this.players = data;
+        console.log(this.teams);
+      }, (error) => {
+        console.log(error);
+      });
+    })
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -10,24 +10,39 @@ export class FootballService {
   constructor( private http: HttpClient) {
 
   }
-
+  // Consulta todas las leagues
   getLeagues(){
     return this.http.get(`${this.url}/leagues`)
   };
 
+  // Consulta la league que tenga el id
   getLeagueById(id: string){
     return this.http.get(this.url +'/leagues?Identificador=' + id)
   }
 
+  // Consulta los teams de una league
   getTeamsById(id: string){
     return this.http.get(this.url +'/teams?Liga=' + id)
   }
 
+
+  // Consulta todos los equipos
   getTeams(){
     return this.http.get(`${this.url}/teams`)
   };
 
+  // Consulta el team que tenga el id
+  getTeamById(id: string){
+    return this.http.get(this.url +'/teams?id=' + id)
+  }
 
+  // Consulta los players de un team
+  getPlayersById(id: string){
+    return this.http.get(this.url +'/players?teamId=' + id)
+  }
+
+
+  // Consulta todos los jugadores
   getPlayers(){
     return this.http.get(`${this.url}/players`)
   };
